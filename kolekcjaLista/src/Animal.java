10ca88746d79420f798b0e4290b74210bb339357
Jakub Burchardt <73552568+VarPx@users.noreply.github.com>
2022-08-23 14:06:27 +0200

    Stworzenie 3 obiektow kot

--- kolekcjaLista/src/Animal.java
+++ kolekcjaLista/src/Animal.java
@@ -1,3 +1,7 @@
 public class Animal {
+    Animal(String name) {
+        this.name = name;
+    }
 
+    public String name = "Filemon";
 }
--- kolekcjaLista/src/Kolekcja.java
+++ kolekcjaLista/src/Kolekcja.java
@@ -1,5 +1,8 @@
 public class Kolekcja {
     public static void main(String[] args) {
+        Animal kot1 = new Animal("Rudy");
+        Animal kot2 = new Animal("Bury");
+        Animal kot3 = new Animal("Bialy");
 
     }
 }
