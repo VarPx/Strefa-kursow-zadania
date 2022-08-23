0fa358dd624da5e7cf766ee1905960f2843e5319
Jakub Burchardt <73552568+VarPx@users.noreply.github.com>
2022-08-23 16:16:55 +0200

    Create ArrayList and complement her

---
+++ KolejkaStos1/src/KolejkaStos1.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class KolejkaStos1 {
+    public static void main(String[] args) {
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("Zielona Wies");
+        arr.add("Poznan");
+        arr.add("Wroclaw");
+        arr.add("Warszawa");
+
+        System.out.println();
+
+        for (String o : arr) {
+            System.out.println(o);
+        }
+    }
+}
