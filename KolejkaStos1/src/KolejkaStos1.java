03f4c98dce22e0f95d9d58f2ddb0e52cd8d95611
Jakub Burchardt <73552568+VarPx@users.noreply.github.com>
2022-08-23 16:31:54 +0200

    Learning Object Collections

--- KolejkaStos1/src/KolejkaStos1.java
+++ KolejkaStos1/src/KolejkaStos1.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class KolejkaStos1 {
     public static void main(String[] args) {
@@ -10,6 +12,31 @@ public class KolejkaStos1 {
 
         System.out.println();
 
+        for (String o : arr) {
+            System.out.println(o);
+        }
+
+        System.out.println("\nArrayList sortowana: ");
+        Collections.sort(arr);
+
+        for (String o : arr) {
+            System.out.println(o);
+        }
+
+        System.out.println();
+        System.out.println(Collections.min(arr));
+
+        System.out.println();
+        System.out.println(Collections.max(arr));
+
+        System.out.println();
+        Collections.reverse(arr);
+        for (String o : arr) {
+            System.out.println(o);
+        }
+
+        System.out.println();
+        Collections.shuffle(arr);
         for (String o : arr) {
             System.out.println(o);
         }
