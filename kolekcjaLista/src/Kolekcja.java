4f675fd72a2ba39404d991bc71fc25ab8454a875
Jakub Burchardt <73552568+VarPx@users.noreply.github.com>
2022-08-23 14:20:35 +0200

    Create ArrayList and display ArrayList

--- kolekcjaLista/src/Kolekcja.java
+++ kolekcjaLista/src/Kolekcja.java
@@ -1,8 +1,23 @@
+import java.util.ArrayList;
+
 public class Kolekcja {
     public static void main(String[] args) {
         Animal kot1 = new Animal("Rudy");
         Animal kot2 = new Animal("Bury");
         Animal kot3 = new Animal("Bialy");
+
+        //tworzenie listy ArrayList nie generycznej
+        ArrayList arr = new ArrayList();
+        arr.add(kot1);
+        arr.add(kot2);
+        arr.add(kot3);
+
+        //wyswietlenie ArrayList
+        System.out.println("--------------");
+        for (Object o : arr) {
+            System.out.println(((Animal)o).name);
+        }
+        System.out.println("--------------");
 
     }
 }
