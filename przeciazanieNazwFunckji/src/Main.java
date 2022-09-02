28a1a6bdaffd23baa7ce8fcef8c1cc2a9c13aa2d
Jakub Burchardt <73552568+VarPx@users.noreply.github.com>
2022-09-02 17:03:59 +0200

    Create metod add on type float

--- przeciazanieNazwFunckji/src/Main.java
+++ przeciazanieNazwFunckji/src/Main.java
@@ -1,13 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(add(5));
+        System.out.println(add(5F));
+        System.out.println(add(10, 2));
+    }
+
+    public static float add(float a) {
+        return --a;
     }
 
     public static int add(int a) {
         return ++a;
     }
 
-    public static int add2(int a, int b) {
+    public static int add(int a, int b) {
         return a + b;
     }
 }
