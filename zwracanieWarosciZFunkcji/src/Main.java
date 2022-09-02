710b6f1c778a21055eb890435d27f7897cdcf1f0
Jakub Burchardt <73552568+VarPx@users.noreply.github.com>
2022-09-02 16:55:22 +0200

    Create metod add and show in monitor result

--- zwracanieWarosciZFunkcji/src/Main.java
+++ zwracanieWarosciZFunkcji/src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
+        int sum = add(2, 5);
+        System.out.println("Suma a + b = " + sum);
+        System.out.println("Suma: " + add(10, 5));
     }
 
-    //funkcja
+    //funkcja/metoda jesli nie jest typem void ktory nic nie zwraca to trzeba dodac slowko "return"
     public static int add(int a, int b) {
-
-        return a;
+        return a + b;
     }
 }
