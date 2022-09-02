9a05b23bfb229dd4195399a175843f6700eb5286
Jakub Burchardt <73552568+VarPx@users.noreply.github.com>
2022-09-02 16:11:21 +0200

    Create method change and add

--- ArgumentyFunkcji/src/Main.java
+++ ArgumentyFunkcji/src/Main.java
@@ -1,9 +1,25 @@
 public class Main {
     public static void main(String[] args) {
+        add(2, 5);
+        add(1, 10);
+        add(5, 7);
 
+        int[] a = {10};
+        change(a);
+        System.out.println("a po wykonaniu change: " + a[0]);
     }
 
-    public static void add(int a, int b){
+    //rezultat, typy proste po wyjściu z funkcji nie zapisują się, natomiast typy złożone zapisują swoją wartość
 
+    //funkcja
+    public static void change(int[] x) {
+        x[0] = 0;
+        x[0]--;
+        System.out.println("Zmienna x = " + x[0]);
+    }
+
+    //funkcja
+    public static void add(int a, int b) {
+        System.out.println("Wynik: " + (a + b));
     }
 }
