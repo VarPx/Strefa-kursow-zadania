d0a05dbd4f667216bc6dafdd2abf372bb8031d7a
Jakub Burchardt <73552568+VarPx@users.noreply.github.com>
2022-09-02 16:58:28 +0200

    Create metod add2

--- przeciazanieNazwFunckji/src/Main.java
+++ przeciazanieNazwFunckji/src/Main.java
@@ -1,9 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(add(5));
     }
 
-    public static int add(int a, int b) {
+    public static int add(int a) {
         return ++a;
+    }
+
+    public static int add2(int a, int b) {
+        return a + b;
     }
 }
